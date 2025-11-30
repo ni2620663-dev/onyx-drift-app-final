e525bc2cde7fc08a38083fea295e0c91c2c9f9a6
Shakib <your_email@example.com>
2025-11-30 14:44:51 +0600

    Final vercel.json configuration and main.jsx link update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app.jsx"; // এখন একটাই App
-import "./index.css";
+import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
