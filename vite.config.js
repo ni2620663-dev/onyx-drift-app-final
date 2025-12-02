991e0604e562ff3f499824f824cba57834a5c523
naimusshakib582-pixel <naimusshakib582@gmail.com>
2025-12-02 18:48:39 +0600

    Enhance Vite config with build and alias settings
    Added build configuration and path aliasing.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: "./", // client folder is root
   plugins: [react()],
+  build: {
+    outDir: "dist", // build output inside client/dist
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  }
 });
-	
